77312aa386a70947f804493183039ec28d6a02bc
Tracy Thompson <traymarkthompson@gmail.com>
2016-05-27 15:17:02 +0000

    added registration form

--- register.php
+++ register.php
@@ -16,7 +16,7 @@
         <label for="name">Name:</label>
             <input type="text" id="name" name="user_name">
         <label for="email">Email:</label>
-            <input type="email" id="email" name="user_email">
+            <input type="email" id="email" name="user_email" placeholder="name@email.com">
         <label for="password">Password:</label>
             <input type="password" id="password" name="user_password">
 
